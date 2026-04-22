844e7c4c8f14868fd67af97caf5dd05b51ce70c0
killerbite95 <88723065+killerbite95@users.noreply.github.com>
2026-04-22 16:08:56 +0200

    chore: update config.example.php

--- config.example.php
+++ config.example.php
@@ -2,6 +2,6 @@
 define('DB_PATH',   __DIR__ . '/data/salones.db');
 
 define('ADMIN_USER', 'admin');
-define('ADMIN_PASS', '$2y$10$lVzBq3i/4v0rfIz0OFxtW.o1SDrExMw7CK62Ut.21EavNoiX/1BCC');
+define('ADMIN_PASS', '$2y$10$Sel0g5A/mbrGl28hIz5ESuXrQ./UTLDNnGNYB833sH1Nxnbj6ytN6');
 
 define('APP_NAME', 'Buscador de Salones · Sportium');
